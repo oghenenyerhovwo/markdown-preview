948e031679265e99c38a1c56c08301429bcf1c7d
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-01-24 20:33:34 +0100

    Done with the CSS styling of both the Editor and Preview Div

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-
+import {useState} from "react";
 function App() {
+  const [textAreaHeight, setTextAreaHeight] = useState(false)
   return (
     <div>
-
+        <div id="editor">
+            <div className="header">
+                <h3>Editor</h3>
+                <h3 onClick={() => setTextAreaHeight(prevHeight => !prevHeight)} className=" icon">/</h3>
+            </div>
+            <textarea style={{height: !textAreaHeight ? "250px": "600px"}} />
+        </div>
+        <div id="preview">
+            <div className="header">
+                <h3>Previewer</h3>
+                <h3 className=" icon">/</h3>
+            </div>
+            <div className="visual-preview"></div>
+        </div>
     </div>
   );
 }
